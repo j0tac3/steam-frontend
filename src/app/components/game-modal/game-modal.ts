9623bfcbcaecd1bf7aae91cd335a7ee2b5f97be6
Andrade . <j0s3.90jr@gmail.com>
2026-04-20 14:51:37 +0200

    actualizado el diseño de la ventana modal

--- src/app/components/game-modal/game-modal.ts
+++ src/app/components/game-modal/game-modal.ts
@@ -1,33 +1,47 @@
-import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, input, output, signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'; // IMPORTANTE
 
 @Component({
   selector: 'app-game-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './game-modal.html',
   styleUrl: './game-modal.scss'
 })
 export class GameModalComponent {
-  @Input() juegoDetalle: any = null;
-  @Input() cargandoDetalle: boolean = false;
-  @Input() esBiblioteca: boolean = false;
+  private sanitizer = inject(DomSanitizer);
+  // Entradas de datos (Inputs)
+  juegoDetalle = input<any>(null);
+  cargandoDetalle = input<boolean>(false);
+  esBiblioteca = input<boolean>(false);
+  
+  // Salida de datos (Output)
+  guardarDiario = output<any>();
+  
+  // Estado visual
+  activeTab = signal<'info' | 'diario'>('info');
 
-  @Output() guardarDiario = new EventEmitter<any>();
+  setRating(valor: number) {
+    const juego = this.juegoDetalle();
+    if (juego) {
+      juego.personal_rating = valor * 2;
+    }
+  }
 
-  // ✅ SOLUCIÓN LIMPIA: Captura el cierre sea cual sea el método (botón, X, Esc, clic fuera)
-  @HostListener('hide.bs.modal')
-  onModalHide() {
-    this.cerrarLimpio();
+  // Método para limpiar el HTML y quitar el error de consola
+  getSafeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
+
+  getStarArray(): boolean[] {
+    const rating = this.juegoDetalle()?.personal_rating || 0;
+    const count = Math.round(rating / 2);
+    return Array(5).fill(false).map((_, i) => i < count);
   }
 
   onGuardar() {
-    this.guardarDiario.emit(this.juegoDetalle);
-  }
-
-  cerrarLimpio() {
-    if (document.activeElement instanceof HTMLElement) {
-      document.activeElement.blur();
-    }
+    this.guardarDiario.emit(this.juegoDetalle());
   }
 }
